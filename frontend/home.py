3ab01e1116c3e115344ce47006a54a363d1fed50
chestergarett <chestergarett@gmail.com>
2024-07-21 09:45:27 +0800

    adding css to intro page

--- frontend/home.py
+++ frontend/home.py
@@ -1,55 +1,130 @@
 import streamlit as st
+import pandas as pd
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from jobs.get_all_firebase_data import load_offline_df
 
-profit = '27.64%'
 
-profit_per_game = {   
-    'mlbb': '42.46%',
-    'hok':'19.17%',
-    'valorant': '0.1%'
-}
+bet_df = load_offline_df('bet_logs')
+bet_df['win_loss_amount'] = bet_df['win_loss_amount'].astype(float)
+bet_df['bet_amount'] = bet_df['bet_amount'].astype(float)
+total_wins = bet_df[bet_df['win_loss_code'] == 'WIN']['win_loss_amount'].sum()
+total_bet_amounts = bet_df[bet_df['win_loss_code'].isin(['WIN', 'LOSS'])]['bet_amount'].sum()
+total_matches = len(bet_df[bet_df['win_loss_code'].isin(['WIN', 'LOSS'])])
+profit = (total_wins) / total_bet_amounts - 1
 
-matches = 62
+profit_per_game = {}
+
+for game in bet_df['game'].unique():
+    game_df = bet_df[bet_df['game'] == game]
+    game_wins = game_df[game_df['win_loss_code'] == 'WIN']['win_loss_amount'].sum()
+    game_bet_amounts = game_df[game_df['win_loss_code'].isin(['WIN', 'LOSS'])]['bet_amount'].sum()
+    if game_bet_amounts > 0:
+        game_profit = (game_wins / game_bet_amounts) - 1
+        profit_per_game[game] = f'{game_profit * 100:.2f}%'
+
 def app():
-    st.title('Holy Grail of Sports Betting')
-    st.write('Abstract')
-    st.write(f'''In traditional financial markets, retail investors face significant challenges: 1) identifying optimal times to BUY, HOLD, and SELL, 2) competing against market whales, and 3) managing biases caused by human emotions. For sports betting, although human emotions still heavily influence decisions however the time variability is negligible and market whales are fewer. This system exploits these factors. To date, the devised system has generated a profit of {profit} from {matches} bets.
-     ''')
-    
-    st.write('Hypothesis')
-    st.write(f'''
-        The system hypothesizes that betting against popular public bets—referred to as "fading the public" in betting terminology—will yield profitable outcomes.
-    ''')
-    
-    st.write('Assumptions & Constraints')
-    st.write(f'''
-        1. Risk Management: An equal bet amount will be placed on each match until a desired profit level is achieved. Subsequently, bet amounts will be gradually increased for the next set of bets.
-        2. Focus on E-sports: E-sports are chosen due to:
-            a. The absence of physical attributes (e.g., height, strength), allowing focus on mental attributes.
-            b. The dynamic nature of the meta, with game developers frequently updating game aspects, leading to frequent shifts in current powerhouses and laggards.
-        3. Game-Specific Assessments: Each game will be evaluated differently, with various subsystems employing different approaches.
-    ''')
-    
-    st.write('Methodology')
-    st.write(f'''
-        The general strategy is to fade the public. However, deviations from this general strategy are employed for different games:
-            Mobile Legends: Bang Bang (MLBB): Fade the public during group/elimination stages; use AI model predictions during playoffs.
-            Honor of Kings (HOK): Fade the public during group/elimination stages.
-            Valorant: Fade the public during group/elimination stages.
-            League of Legends (LOL): Fade the public during group/elimination stages.
-    ''')
+    st.markdown(
+        """
+        <style>
+        .subheader {
+            font-size: 1.5em;
+            font-weight: bold;
+        }
+        .highlight {
+            color: green;
+            font-weight: bold;
+        }
+        .game-name {
+            font-weight: bold;
+        }
+        .justified-text {
+            text-align: justify;
+            margin: 20px;
+        }
+        </style>
+        """, unsafe_allow_html=True
+    )
 
-    st.write('Results & Findings')
+    st.title('Holy Grail of Sports Betting')
+    st.write('### **Introduction**')
+    st.write(f'''<div class='justified-text'>
+             In traditional financial markets, retail investors face significant challenges: 
+                1) identifying optimal times to BUY, HOLD, and SELL, 
+                2) competing against market whales, and
+                 3) managing biases caused by human emotions. 
+                For sports betting, although human emotions still heavily influence decisions - the time variability is negligible and market whales are fewer. 
+                This system exploits these factors. To date, the devised system has generated 
+                a profit of <span class='highlight'>{profit: .2%}</span> from <span class='highlight'>{total_matches}</span> bets.
+            </div>
+     ''',unsafe_allow_html=True)
+    
+    st.write('### **Hypothesis**')
     st.write(f'''
-       Turns out, the public often get it right. As such, we need to spot what they believe is wrong and we bet with that team. After, post bet-analysis and match simulations current profit stands at {profit}.
-        Breakdown per game are as follows:
-    ''')
+        <div class='justified-text'>
+        The system hypothesizes that betting against popular public bets—referred to as "fading the public" in betting terminology—will yield profitable outcomes.
+        </div>
+    ''',unsafe_allow_html=True)
+    
+    st.write('### **Assumptions & Constraints**')
+    st.write(f'''
+        <div class='justified-text'>
+            <ul>     
+                <li><strong> 1. Risk Management </strong>: An equal bet amount will be placed on each match until a desired profit level is achieved. Subsequently, bet amounts will be gradually increased for the next set of bets. </li>
+                <li><strong> 2. Focus on E-sports</strong>: E-sports are chosen due to:
+                    <ul>
+                        <li><em>a</em>. The absence of physical attributes (e.g., height, strength), allowing focus on mental attributes. </li>
+                        <li><em>b</em>. The dynamic nature of the meta, with game developers frequently updating game aspects, leading to frequent shifts in current powerhouses and laggards. </li>
+                    </ul>
+                </li>
+               <li><strong>3. Game-Specific Assessments</strong>: Each game will be evaluated differently, with various subsystems employing different approaches.</li>
+            </ul>
+        </div>
+    ''',unsafe_allow_html=True)
+    
+    st.write('### **Methodology**')
+    st.write('''
+        <div class='justified-text'>
+        The general strategy is to fade the public. However, deviations from this general strategy are employed for different games:
+             <ul>     
+                <li><span class='game-name'>Mobile Legends: Bang Bang (MLBB)</span>: Fade the public during group/elimination stages; use AI model predictions during playoffs. </li>
+                <li><span class='game-name'>Honor of Kings (HOK)</span>: Fade the public during group/elimination stages.</li>
+                <li><span class='game-name'>Valorant</span>: Fade the public during group/elimination stages.</li>
+                <li><span class='game-name'>League of Legends (LOL)</span>: Fade the public during group/elimination stages.</li>
+             </ul>
+        </div>
+    ''', unsafe_allow_html=True)
+
+    st.write('### **Results & Findings**')
+    st.write(f'''
+        <div class='justified-text'>
+            Turns out, the public sometimes get it right. 
+            As such, being selective when we bet against the public and further filter down prospective bets will maximize profit results. 
+            After, post bet-analysis and match simulations current profit stands at <span class='highlight'>{profit:.2%}</span>.
+            Breakdown per game are as follows:
+        </div>
+        ''', unsafe_allow_html=True)
+    
     for key, value in profit_per_game.items():
-        st.write(f'{key} with profit of {value}')
+        st.write(f'<div class="justified-text"><span class="game-name">{key}</span> with profit of <span class="highlight">{value}</span></div>', unsafe_allow_html=True)
 
     st.write('''
-       The best bet performance is observed in MLBB, attributed to the author's in-depth knowledge of the game and the incorporation of qualitative factors and simulations into decision-making models. For other games, bets are placed primarily based on odds. To enhance yields without detailed game knowledge, an odds simulation model was developed, identifying bets with odds of 2 and 3 as yielding the best results.
-    ''')
+        <div class='justified-text'>
+        The best bet performance is observed in <span class="game-name">MLBB</span>, attributed to the author's in-depth knowledge of the game and the incorporation of qualitative factors 
+        and simulations into decision-making models. 
+        For other games, bets are placed primarily based on odds. 
+             To enhance yields without detailed game knowledge, an odds simulation model was developed, 
+             identifying bets with odds of <span class='highlight'>2 and 3</span> as yielding the best results.
+        </div>
+    ''',unsafe_allow_html=True)
 
-    st.write('Conclusion')
-    st.write(f'''The findings indicate consistent profitability in the system. This research underscores the importance of strategic betting against public sentiment and the value of game-specific knowledge and simulation models in maximizing betting profits.''')
-    return
+    st.write('### **Conclusion**')
+    st.write(f'''
+             <div class='justified-text'>
+             The findings indicate consistent profitability in the system. 
+             This research underscores the importance of strategic betting against public sentiment and the value of game-specific knowledge and simulation models 
+             in maximizing betting profits.
+             </div>
+             ''',unsafe_allow_html=True)
+    
